fc3e517ab88612a85ff49027ba48eba97f79c5b5
嘉然今天敲代码 <14633797+foodie_diana@user.noreply.gitee.com>
2024-07-04 10:51:55 +0800

    Version 3.5sqlupdate!!!

--- ruoyi-ui/src/api/menu.js
+++ ruoyi-ui/src/api/menu.js
@@ -6,12 +6,19 @@ export const getRouters = () => {
     url: '/getRouters',
     method: 'get'
   })
-  }
+}
 
-  // 查询院校历年平均薪资
-  export function getCollegeYearlyAvgSalaries() {
-    return request({
-      url: '/employment/employment_analysis/collegeYearlyAvgSalaries',
-      method: 'get'
-    })
+// 查询院校历年平均薪资
+export function getCollegeYearlyAvgSalaries() {
+  return request({
+    url: '/employment/employment_analysis/collegeYearlyAvgSalaries',
+    method: 'get'
+  })
+}
+
+export function getPopularProvinces() {
+  return request({
+    url: '/employment/analysis/popularProvinces',
+    method: 'get'
+  });
 }
